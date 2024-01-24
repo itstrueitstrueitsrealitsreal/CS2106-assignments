97a0f95659bfde7279e6997c103271b29585171e
Kenneth Seet <kenurfseet@gmail.com>
2024-01-24 19:44:26 +0800

    completed part 1

--- CS2106Lab1/lab1programs/part1/lab1p1a.c
+++ CS2106Lab1/lab1programs/part1/lab1p1a.c
@@ -19,9 +19,9 @@ int main() {
     printf("Calling func with value 6: %d\n", func(6));
     printf("Now setting fptr to point to func.\n");
     fptr = func;
-    printf("Caling fptr with value 6: %d\n", fptr(6));
+    printf("Calling fptr with value 6: %d\n", fptr(6));
 
-    printf("\nNow caling func2 which returns the address of global variable y: %p\n", func2());
+    printf("\nNow calling func2 which returns the address of global variable y: %p\n", func2());
     printf("Pointing pfptr to func2.\n");
     pfptr = func2;
     printf("Now calling fpfptr: %p\n", pfptr());
--- CS2106Lab1/lab1programs/part1/queue.h
+++ CS2106Lab1/lab1programs/part1/queue.h
@@ -5,3 +5,18 @@
 
 // declare prototypes below
 
+void enq(double);
+
+double deq();
+
+double sum(double, double);
+
+double prod(double, double);
+
+void clear_sum(double*);
+
+void clear_prod(double*);
+
+double reduce();
+
+double flex_reduce(void (*clear)(double *), double (*op)(double, double));
