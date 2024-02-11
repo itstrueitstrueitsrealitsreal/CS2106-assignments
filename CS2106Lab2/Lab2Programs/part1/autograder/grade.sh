0ca8e001d1a91da1d38d1d96ee915defd0b10bfc
Kenneth Seet <kenurfseet@gmail.com>
2024-02-12 00:41:15 +0800

    Partial grade.sh complete

--- CS2106Lab2/Lab2Programs/part1/autograder/grade.sh
+++ CS2106Lab2/Lab2Programs/part1/autograder/grade.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
 # Check if we have enough arguments
-
+if [[ $# -ne 1 ]]; then
+    echo "Usage: ./grade.sh <filename>"
+else
 # Delete temporary files
-
+rm ./ref/*.out
 # Compile the reference program
-
+gcc ./ref/sum.c ./ref/utils.c -o "$1"
 # Generate reference output files
-
+for i in ./ref/*.in; do
+    ./"$1" < "$i" > "${i%.in}".out
+done
 # Now mark submissions
-
+echo -e "Test date and time: $(date +%A), $(date +%d) $(date +%B) $(date +%Y), $(date +%T)\n" > results.out
 #
 # Note: See Lab02Qn.pdf for format of output file. Marks will be deducted for missing elements.
 #
 
 # Iterate over every submission directory
+for student in ./subs/*; do
     # Compile C code
     # Print compile error message to output file
+    gcc "$student"/sum.c "$student"/utils.c -o "$student/$1" >> results.out
+    max_score=$(ls ref/*.in | wc -l)
+    score=0
     # Generate output from C code using *.in files in ref
-    # Compare with reference output files  and award 1 mark if they are identical
-# print score for student
+    for i in ./ref/*.in; do
+        ref_output="${i%.in}.out"
+        student_output="$("$student/${1}" < "$i")"
+        if diff -q "$student_output" "$ref_output" &>/dev/null; then
+            ((score++))
+        fi
+    done
+    # Compare with reference output files and award 1 mark if they are identical
+    # print score for student
+    echo -e "Directory ${student##*/} score ${score} / ${max_score}" >> results.out
+
+done
 # print total files marked.
-    
+echo -e "\nProcessed $(ls ./subs/ | wc -l) files." >> results.out
+fi
