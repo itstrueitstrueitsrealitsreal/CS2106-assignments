182584eb5cef42d6056779aa15433086b9663a11
Kenneth Seet <kenurfseet@gmail.com>
2024-02-12 15:50:29 +0800

    Complete 2.7 partially

--- CS2106Lab2/Lab2Programs/part2/lab2p2f.c
+++ CS2106Lab2/Lab2Programs/part2/lab2p2f.c
@@ -16,5 +16,31 @@ int main() {
     // WITHOUT using | and > from the shell.
     //
 
+    // set up pipe
+    int p[2];
+    // This creates a pipe. p[0] is the reading end,
+    // p[1] is the writing end.
+
+    if (pipe(p) < 0) {
+        perror("lab2p2f: ");
+    }
+
+    // Send message from slow to talk
+    if (fork() != 0) {
+        close(p[0]);
+        dup2(p[1], STDOUT_FILENO);
+        close(p[1]);
+        char *args[] = {"./slow", "5", NULL};
+        execvp(args[0], args);
+    } else {
+        close(p[1]);
+        int fp_out = open("./results.out", O_CREAT | O_WRONLY);
+        dup2(fp_out, STDOUT_FILENO);
+        close(fp_out);
+        close(p[0]);
+        execlp("./talk", "./talk", NULL);
+    }
+
+    return 0;
 }
 
