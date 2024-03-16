932c33a82434d498272c1052a61f3a647336ff08
Kenneth Seet <kenurfseet@gmail.com>
2024-03-16 16:15:09 +0800

    part 10

--- CS2106lab3/part1/lab3p1-shm-counter.c
+++ CS2106lab3/part1/lab3p1-shm-counter.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <semaphore.h>
 
 #define NUM_CHILDREN 5
 
@@ -13,23 +14,51 @@ int main()
 
     int i;
     int *shm;
+    sem_t semaphores[NUM_CHILDREN];
 
     pid_t pid;
-    // create Shared Memory Region
-    int shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
-    if (shmid == -1)
+
+    // create Shared Memory Region for counter variable
+    int ctrid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    if (ctrid == -1)
     {
         perror("shmget");
         exit(EXIT_FAILURE);
     }
     // attach the shared memory region to this process
-    shm = shmat(shmid, NULL, 0);
+    shm = shmat(ctrid, NULL, 0);
     if (shm == (int*) -1)
     {
         perror("shmat");
         exit(EXIT_FAILURE);
     }
+
+    // create Shared Memory Region for semaphores
+    int semid = shmget(IPC_PRIVATE, NUM_CHILDREN * sizeof(sem_t), IPC_CREAT | 0600);
+    if (semid == -1)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+    // attach the shared memory region to this process
+    sem_t *sems = shmat(semid, NULL, 0);
+    if (sems == (sem_t*) -1)
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+
+    // initialise counter to 0
     *shm = 0;
+    // Initialize semaphores
+    for (i = 0; i < NUM_CHILDREN; i++)
+    {
+        if (sem_init(&semaphores[i], 1, 0) == -1)
+        {
+            perror("sem_init");
+            exit(EXIT_FAILURE);
+        }
+    }
     for (i = 0; i < NUM_CHILDREN; i++)
     {
         pid = fork();
@@ -43,6 +72,15 @@ int main()
     }
     else if (pid == 0)
     {
+        // Wait for previous child processes to finish their turn
+        if (i > 0)
+        {
+            if (sem_wait(&semaphores[i - 1]) == -1)
+            {
+                perror("sem_wait");
+                exit(EXIT_FAILURE);
+            }
+        }
         // Child process
         printf("Child %d starts\n", i + 1);
         // Simulate some work
@@ -53,7 +91,16 @@ int main()
             fflush(stdout);
             usleep(250000);
         }
+
+        // Release semaphore for next child process
+        if (sem_post(&semaphores[i + 1]) == -1)
+        {
+            perror("sem_post");
+            exit(EXIT_FAILURE);
+        }
+
         printf("Child %d finishes with counter %d\n", i + 1, *shm);
+        // release the turn
         exit(EXIT_SUCCESS);
     }
 
@@ -65,14 +112,31 @@ int main()
 
     // Print the final value of the counter
     printf("Final counter value: %d\n", *shm);
-    // Detach the shared memory segment
+    // Detach the shared memory segment for counter
     if (shmdt(shm) == -1) {
         perror("shmdt");
         exit(EXIT_FAILURE);
     }
 
-    // Remove the shared memory segment
-    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+    // Remove the shared memory segment for counter
+    if (shmctl(ctrid, IPC_RMID, NULL) == -1) {
+        perror("shmctl");
+        exit(EXIT_FAILURE);
+    }
+
+    // Destroy the semaphores
+    for (int i = 0; i < NUM_CHILDREN; i++)
+    {
+        sem_destroy(&sems[i]);
+    }
+
+    // Detach the shared memory segment for semaphores
+    if (shmdt(sems) == -1) {
+        perror("shmdt");
+        exit(EXIT_FAILURE);
+    }
+    // Remove the shared memory segment for semaphores
+    if (shmctl(semid, IPC_RMID, NULL) == -1) {
         perror("shmctl");
         exit(EXIT_FAILURE);
     }
