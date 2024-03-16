6ed2be3f48e57a7751332d2d4b0000a631c321f2
Kenneth Seet <kenurfseet@gmail.com>
2024-03-16 14:05:31 +0800

    Complete qn1.4

--- CS2106lab3/part1/lab3p1-shm-counter.c
+++ CS2106lab3/part1/lab3p1-shm-counter.c
@@ -3,15 +3,33 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define NUM_CHILDREN 5
 
 int main()
 {
 
-    int counter = 0, i;
-    pid_t pid;
+    int i;
+    int *shm;
 
+    pid_t pid;
+    // create Shared Memory Region
+    int shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    if (shmid == -1)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+    // attach the shared memory region to this process
+    shm = shmat(shmid, NULL, 0);
+    if (shm == (int*) -1)
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+    *shm = 0;
     for (i = 0; i < NUM_CHILDREN; i++)
     {
         pid = fork();
@@ -30,12 +48,12 @@ int main()
         // Simulate some work
         for (int j = 0; j < 5; j++)
         {
-            counter++;
-            printf("Child %d increment counter %d\n", i + 1, counter);
+            (*shm)++;
+            printf("Child %d increment counter %d\n", i + 1, *shm);
             fflush(stdout);
             usleep(250000);
         }
-        printf("Child %d finishes with counter %d\n", i + 1, counter);
+        printf("Child %d finishes with counter %d\n", i + 1, *shm);
         exit(EXIT_SUCCESS);
     }
 
@@ -45,8 +63,19 @@ int main()
         wait(NULL);
     }
 
-    // Print the final value of thecounter
-    printf("Final counter value: %d\n", counter);
+    // Print the final value of the counter
+    printf("Final counter value: %d\n", *shm);
+    // Detach the shared memory segment
+    if (shmdt(shm) == -1) {
+        perror("shmdt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Remove the shared memory segment
+    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+        perror("shmctl");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
